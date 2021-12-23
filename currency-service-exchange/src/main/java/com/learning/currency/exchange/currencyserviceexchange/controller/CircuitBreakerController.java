d64fee7d711e865033586a2c13f56b0890bf567e
Balaji Soundarrajan <balaji.pacific@gmail.com>
2021-12-23 15:03:53 -0500

    Updating code with rate limiter

--- currency-service-exchange/src/main/java/com/learning/currency/exchange/currencyserviceexchange/controller/CircuitBreakerController.java
+++ currency-service-exchange/src/main/java/com/learning/currency/exchange/currencyserviceexchange/controller/CircuitBreakerController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 
 /**
@@ -24,12 +25,16 @@ public class CircuitBreakerController {
 	@GetMapping("/sample-api")
 	@Retry(name = "sample-api", fallbackMethod="hardcodedResponse")
 	public String sampleAPI() {
-	
 		logger.info("Entering Sample API");
-		
 		ResponseEntity<String> responseEntity = new RestTemplate().getForEntity("http://localhost:8080/dummysite", String.class);
-		
 		return responseEntity.getBody();
+	}
+	
+	@GetMapping("rate-limiter")
+	@RateLimiter(name="default")
+	public String sampleAPIRateLimiter() {
+		logger.info("Rate Limiter");
+		return "rate-limiter";
 	}
 	
 	public String hardcodedResponse(Exception ex) {
